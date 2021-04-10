97953f90e4e3c8ce28b9a327e242c1aa25a801ce
lafernando <lafernando@gmail.com>
2021-04-09 17:50:27 -0700

    Update

---
+++ Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        int n = in.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int result = solve();
+            System.out.println("Case #" + i + ": " + result);
+        }
+    }
+
+    private static int solve() {
+        int result = 0;
+        return result;
+    }
+
+}
